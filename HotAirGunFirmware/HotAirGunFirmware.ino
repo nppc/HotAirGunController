8d1974edf640f13e76c0bd629a3c6fa680bf5219
Palonen <ppalonen@bethel.jw.org>
2018-01-15 11:14:30 +0100

    Testing Heater element

--- HotAirGunFirmware/HotAirGunFirmware.ino
+++ HotAirGunFirmware/HotAirGunFirmware.ino
@@ -1,4 +1,4 @@
-#include "Adafruit_MAX31855.h"
+#define DEBUG
 
 #define FAN_PIN     6 // Pin for controlling fan via PWM
 #define LED_PIN     13
@@ -7,11 +7,21 @@
 #define MAXCS_PIN   3
 #define MAXDO_PIN   4
 
-#define PID_WINDOWSIZE  500 * 8
-unsigned long soft_pwm_millis=millis();
+#define TIMER_MULTIPLIER  8 // if timer has normal (x64) prescaller, then 1
 
+#define PID_WINDOWSIZE  500 // software PWM period for Heater
+
+// some macros
+#define mdelay(val) delay(val * TIMER_MULTIPLIER)
+#define mmillis() (millis() / TIMER_MULTIPLIER)
 #define H_ON   digitalWrite(HEATER_PIN,HIGH);digitalWrite(LED_PIN, HIGH);
 #define H_OFF   digitalWrite(HEATER_PIN,LOW);digitalWrite(LED_PIN, LOW);
+
+
+unsigned long soft_pwm_millis = mmillis();
+unsigned long serial_ms = mmillis();
+
+int outVal = 0;
 
 
 void setup() {
@@ -30,34 +40,41 @@ void setup() {
   // TCCR0B = TCCR0B & 0b11111000 | 0x03; //x64 - default timer speed
   TCCR0B = TCCR0B & 0b11111000 | 0x02; // x8
   
+  #ifdef DEBUG
   analogWrite(FAN_PIN,255); //Full for testing Heater
+  #endif
 }
 
 void loop() {
   
-  Serial.print("Int. Temp = ");
-  Serial.println(readMAX31855());
-  delay(1000 * 8);
+	#ifdef DEBUG
+	if (serial_ms+1000 < mmillis()) {
+		serial_ms = mmillis();
+		Serial.print(F("Temp = "));
+		Serial.println(readMAX31855());
+	}	
+	#endif
 
     if (Serial.available() > 0) {
-      char incomingByte = Serial.read();
-      incomingByte=constrain(incomingByte,48,57);
-      int outVal=map(incomingByte,48,57,0,100);
-      doSoftwarePWM((uint16_t)outVal); 
-      Serial.print(": I = ");
-      Serial.print(incomingByte);
-      Serial.print(", O = ");
-      Serial.println(outVal);
+		char incomingByte = Serial.read();
+		incomingByte=constrain(incomingByte,48,57);
+		outVal=map(incomingByte,48,57,0,100);
+		Serial.print(F(": I = "));
+		Serial.print(incomingByte);
+		Serial.print(F(", O = "));
+		Serial.println(outVal);
     }
+	if(outVal>0){doSoftwarePWM((uint16_t)outVal);}else{H_OFF;}
+
 }
 
 
 // PWM output 
 void doSoftwarePWM(uint16_t pwm_val){
-  if (millis() - soft_pwm_millis > (PID_WINDOWSIZE))
+  if (mmillis() - soft_pwm_millis > (PID_WINDOWSIZE))
     { //time to shift the Relay Window
     soft_pwm_millis += PID_WINDOWSIZE;
     }
-  if (pwm_val < millis() - soft_pwm_millis){H_OFF}else{H_ON}
+  if (pwm_val < mmillis() - soft_pwm_millis){H_OFF}else{H_ON}
 }
 
